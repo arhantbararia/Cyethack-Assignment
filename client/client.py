bb803a0d2a3af7d5d05865d5351cbd6b0c878175
arhantbararia <arhantbararia@gmail.com>
2023-10-24 23:48:04 +0530

    made client more flexible

--- client/client.py
+++ client/client.py
@@ -3,13 +3,16 @@ from Client.connection import clientConnection
 from Client.handleConnection import handleConnection
 
 
+SERVER_IP = input("ENTER SERVER IP")
+SERVER_PORT = int(input("ENTER SERVER PORT"))
+
 
 
 
 if __name__ == "__main__":
    my_socket = clientConnection()
 
-   my_socket.createConnection("35.154.51.127" , 8080)
+   my_socket.createConnection(SERVER_IP , SERVER_PORT)
 
    handleConnection(my_socket)
    print("Closing Connection")
